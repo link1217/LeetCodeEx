456296b2d97b3fb3b7e7554a3e2ab0c255f8be92
watcher <lanrengufeng@qq.com>
2018-07-10 22:04:17 +0800

    leetcode初始化

---
+++ src/leetcode/Solution.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * Given a non-empty string s and a dictionary wordDict containing a list of
+	 * non-empty words, add spaces in s to construct a sentence where each word
+	 * is a valid dictionary word. Return all such possible sentences.
+	 * 
+	 * Note:
+	 * 
+	 * The same word in the dictionary may be reused multiple times in the
+	 * segmentation. You may assume the dictionary does not contain duplicate
+	 * words.
+	 * 
+	 * @param s
+	 * @param wordDict
+	 * @return
+	 */
+	public List<String> wordBreak(String s, List<String> wordDict) {
+		ArrayList<String> list = new ArrayList<String>();
+		
+		
+		
+		return list;
+	}
+
+}
