96964209e495bfffe3152c915350b6630b836ff4
Watcher <lanrengufeng@163.com>
2018-12-12 10:48:48 +0800

    update 106

--- src/leetcode2/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/leetcode2/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -6,7 +6,6 @@ import others.TreeNode;
  * 106. Construct Binary Tree from Inorder and Postorder Traversal
  */
 public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
-    private int inIndex = 0, posIndex = 0;
 
     /**
      * 递归版，耗时1ms
@@ -19,6 +18,8 @@ public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
         return buildTree(inorder, postorder, Integer.MAX_VALUE);
     }
 
+    private int inIndex = 0, posIndex = 0;
+
     private TreeNode buildTree(int[] inorder, int[] postorder, int val) {
         if (inIndex >= inorder.length || inorder[inorder.length - inIndex - 1] == val)
             return null;
@@ -26,6 +27,22 @@ public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
         root.right = buildTree(inorder, postorder, root.val);
         inIndex++;
         root.left = buildTree(inorder, postorder, val);
+        return root;
+    }
+
+    public TreeNode buildTree2(int[] inorder, int[] postorder) {
+        inIndex = inorder.length - 1;
+        posIndex = postorder.length - 1;
+        return buildTree2(inorder, postorder, Integer.MAX_VALUE);
+    }
+
+    private TreeNode buildTree2(int[] inorder, int[] postorder, int val) {
+        if (inIndex < 0 || inorder[inIndex] == val)
+            return null;
+        TreeNode root = new TreeNode(postorder[posIndex--]);
+        root.right = buildTree2(inorder, postorder, root.val);
+        inIndex--;
+        root.left = buildTree2(inorder, postorder, val);
         return root;
     }
 }
